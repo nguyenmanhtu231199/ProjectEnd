955e9690a9cbc00d8882a50b14d526186c58fe74
nguyenmanhtu231199 <NGUYENMANHTU231199@GMAIL.COM>
2023-03-21 10:33:36 +0700

    update

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,3 +1,4 @@
+import DAO.AccountDao;
 import Service.AuthenService;
 
 import java.util.Scanner;
@@ -10,26 +11,37 @@ public class Application {
 
     private static void showMenu(){
         System.out.println("---QUAN LY NHAN SU---");
+        System.out.println("1. ");
     }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        if(isLoginSuccess== false){
-            // Dang nhap truoc
+        System.out.println(" --- LOGIN--- ");
+        int count = 3;
+        while (count != 0){
+            count = count -1;
             System.out.print("Nhap username: ");
             String username = in.nextLine();
             System.out.print("Nhap password: ");
             String password = in.nextLine();
-
-            // Kiem tra xem dang nhap dc ko?
+            // verify
             isLoginSuccess = authenService.login(username, password);
+            if(isLoginSuccess == true) {
+                break;
+            }
+            if(count==0){
+                continue;
+            }else {
+                System.out.printf("Bạn còn %d lần đăng nhập \n ", count);
+            }
         }
-
         if(isLoginSuccess == false){
-            System.out.println("Dang nhap that bai");
+            System.out.println("Dang nhap that bai, bạn đã hết lượt");
             System.exit(0);
         }
-
+        else {
+            System.out.println("Đăng nhập thành công");
+        }
         // Dang nhap thanh cong
         showMenu();
 
